470a52582c7eb09c68205818b5ded94612ff0032
İlhan Subaşı <ilhan@ilhan.name>
2019-01-12 10:00:44 +0300

    Factorial

--- ilhan.erl
+++ ilhan.erl
@@ -1,7 +1,11 @@
 -module(ilhan).
 -author("Ilhan Subasi").
 -export([fibonacci/1]).
+-export([factorial/1]).
 
 fibonacci(0) -> 0;
 fibonacci(1) -> 1;
 fibonacci(N) -> fibonacci(N-1) + fibonacci(N-2).
+
+factorial(0) -> 1;
+factorial(N) -> N * factorial(N-1).
